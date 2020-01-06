7c7ad7d360f3d0b83f0e70fe7cc157eb76204559
zaursheki <zibrahimov95@gmail.com>
2020-01-07 03:04:54 +0400

    React Native Fetch Contacts

---
+++ App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { View, Button, PermissionsAndroid } from 'react-native';
+var Contacts = require('react-native-contacts')
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    }
+  }
+
+  fetchContacts = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        {
+          title: 'React Native Contact Permission',
+          message: 'React Native needs access to your contacts',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        
+        Contacts.getAll((err, contacts) => {
+          if (err && err.type === 'permissionDenied') {
+            console.log("permissionDenied");
+          } else {
+            console.log("contacts: ", contacts);
+            console.log("=====================");
+            contacts.map(c =>
+              c.phoneNumbers.map(p =>
+                console.log(c.displayName + " - " + p.number)
+              )
+            )
+          }
+          
+        })
+      } else {
+        console.log("error");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Button
+          title="Scan Contacts"
+          onPress={() => this.fetchContacts()}
+        />
+      </View>
+    );
+
+  }
+
+}
